b71363488118e47b7c1a2647272d00789fb3299d
nicholas301205 <s22310321@student.unklab.ac.id>
2025-11-01 21:53:21 +0800

    Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Logo from "./Logo";
+import Header from "./Header";
+import Home from "./Home";
+import Footer from "./Footer";
 
 const App = () => {
-  return(
-    <>
-    <div className="">
-      <Logo/>
-      <Header/>
-      <Home/>
-      <Footer/>
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+      <Logo />
     </div>
-    </>
-  )
-}
+  );
+};
 
 export default App;
--- src/Logo/index.jsx
+++ src/Logo/index.jsx
@@ -1,20 +1,19 @@
 const Logo = () => {
   return (
-<>
-{/* Nav & Logo */}  
-  <div className="bg-orange-500 m-0 p-5 flex items-center h-20 justify-between">
-    {/* Logo */}
-    <img src="IMG_20251101_132548.png" alt="Logo" className="h-20 w-auto"/>
+    <>
+      {/* Nav & Logo */}  
+      <div className="bg-orange-500 m-0 p-5 flex items-center h-20 justify-between">
+        {/* Logo */}
+        <img src="/IMG_20251101_132548.png" alt="Logo" className="h-16 w-auto object-contain"/>
 
-    {/* Navbar */}  
-    <div className="flex space-x-8">
-      <a href="#" class="text-sm/6 font-semibold text-white">Home</a>
-      <a href="#" class="text-sm/6 font-semibold text-white">Search</a>
-      <a href="#" class="text-sm/6 font-semibold text-white">Category</a>
-    </div>
-  </div>
-</>
-
+        {/* Navbar */}  
+        <div className="flex space-x-8">
+          <a href="#" className="text-sm/6 font-semibold text-white">Home</a>
+          <a href="#" className="text-sm/6 font-semibold text-white">Search</a>
+          <a href="#" className="text-sm/6 font-semibold text-white">Category</a>
+        </div>
+      </div>
+    </>
   );
 };
 
